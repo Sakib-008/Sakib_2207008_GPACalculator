ddde75cf400cb2925f6e0acfaf6936ef9c52e8b9
Sakib-008 <kzsakibcr@gmail.com>
2025-11-17 03:02:59 +0600

    Implemented the CourseController logic partially

--- src/main/java/com/example/gpa_calculator/CourseController.java
+++ src/main/java/com/example/gpa_calculator/CourseController.java
@@ -1,5 +1,38 @@
 package com.example.gpa_calculator;
 
-public class CourseController {
 
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class CourseController {
+    public TextField courseName, courseCode, courseCredit, teacher1, teacher2;
+    public ComboBox<String> grade;
+    public Button calculateBtn, addCouresBtn;
+
+    public void initialize(){
+        grade.setItems(FXCollections.observableArrayList(
+                "A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D", "F"
+        ));
+    }
+
+    public void addCourse(){
+        try {
+            String name = courseName.getText();
+            String code = courseCode.getText();
+            double credits = Double.parseDouble(courseCredit.getText());
+            String t1 =  teacher1.getText();
+            String t2 = teacher2.getText();
+            String g =  grade.getValue();
+
+        }
+        catch (Exception e){
+
+        }
+    }
+
+    public void showResult(ActionEvent actionEvent) {
+    }
 }
